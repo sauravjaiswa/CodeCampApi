b50cd66353cd4663a1a1bd340957fa73dedd683d
sauravjaiswa <sauravjaiswal520@gmail.com>
2021-05-17 01:56:02 +0530

    Implented Functional APIs

---
+++ Controllers/OperationsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeCampApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public OperationsController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)configuration;
+                root.Reload();
+                return Ok();
+            }
+            catch
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+    }
+}
